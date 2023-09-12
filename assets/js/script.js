201dbf5f069101f798d5a9203ae3797d282ec610
0sugo <josugo26@gmail.com>
2023-09-13 01:17:48 +0300

    Add form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,3 +144,33 @@ const initSlider = function (currentSlider) {
 }
 
 for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
+
+// Form validation
+const email = document.getElementById('email');
+const form = document.getElementById('contact-form');
+const errorMsg = document.getElementById('form-error');
+const name = document.getElementById("name");
+const message = document.getElementById("message");
+
+form.addEventListener('submit', (event) => {
+
+localStorage.setItem('name' , document.getElementById("name").value);
+localStorage.setItem('email' , document.getElementById("email").value);
+localStorage.setItem('message' , document.getElementById("message").value);
+
+const setName = localStorage.getItem('name');
+const setEmail = localStorage.getItem('email');
+const setMessage = localStorage.getItem('message');
+
+
+  if (email.value !== email.value.toLowerCase()) {
+    errorMsg.style.visibility = 'visible';
+    event.preventDefault();
+  } else if (email.validity.typeMismatch) {
+    errorMsg.textContent = 'Please enter a valid email address';
+    errorMsg.style.validity = 'visible';
+    event.preventDefault();
+  } else {
+    errorMsg.style.visibility = 'hidden';
+  }
+});
